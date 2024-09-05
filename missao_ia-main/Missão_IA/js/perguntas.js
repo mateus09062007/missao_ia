af9e49690b93cbbd93618da0902517fd5643c47a
mateus tavares <117365720+mateus09062007@users.noreply.github.com>
2024-09-05 11:23:44 -0300

    Add files via upload

---
+++ missao_ia-main/Missão_IA/js/perguntas.js
@@ -0,0 +1,172 @@
+export const perguntas = [
+    {
+        enunciado: "Depois de muito tempo dentro da cela, voces ja estao com fome.Mas a porta finalmente se abre e dela sai um homem que nao fala sua lingua. oque voce faz?",
+        alternativas: [
+            {
+                texto: "tenta conversar com ele por gestos",
+                afirmacao: [
+                    "No primeiro contato ele ri por ver voce tentar falar com ele por gestos, mas comeca a falar em frances",
+                    "ao te ver conversando por gestos ele vai embora por te achar bobo falando com sinais. depois de um dia volta falando em frances"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "por ter conhecimento em diversas linguas, vai testando todas ate achar alguma que ele fale",
+                afirmacao: [
+                    "ele te olha surpreso por seu conhecimento, e te responde finalmente em frances",
+                    "ele acha engracada sua tentativa de se comunicar, e comeca a falar com voce em frances"
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "depois desse primeiro contato ele apresenta a voce o submarino, com as tecnologias mais avancadas existentes e a fartura do lugar, mas ainda um pouco severo, ja que a pouco estavam tentando caca-lo",
+        alternativas: [
+            {
+                texto: "voce presta atencao em tudo procurando possiveis pontos de fuga, mas tenta parecer simpatico para ganhar a confianca do capitao e nao levantar suspeitas",
+                afirmacao: [
+                    "depois de sair da cela e ser apresentado o submarino, apesar de procurar uma fuga, parece tudo muito bem guardado, nao parece ter esperancas de sair do submarino",
+                    "depois de sair da cela, o capitao nemo te apresenta o submarino, querendo fugir procura um ponto de fuga, a unica coisa que acha e uma abertura para um bote de pesca flutuante que pode ser usado como esconderijo e fuga",
+                    "entao saindo da cela nemo te apresenta o submarino, voce ja querendo escapar repara em todos os lugares, existem poucas expectativas de fuga mas voce nao perde as esperancas, ainda vai encontrar um jeito"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "voce presta atencao em tudo, nunca tinha visto tamanha tecnologia, se encanta pelo lugar, passando a gostar tanto do lugar, que nao quer ir mais embora, mas sim aproveitar as maravilhas que o submarino proporciona",
+                afirmacao: [
+                    "ao sair da prisao e ser apresentado ao submarino, se encanta com a tecnologia e oportunidades de estudo, nao quer sair dali tao cedo,afinal pode viver uma vida tranquila a bordo do submarino",
+                    "apesar da conturbacao inicial ao sair da cela e ser apresentado ao submarino, se ve absorto nas maravilhas do lugar que ate se esquece do acontecido, e mergulha nas experiencias do ambiente",
+                    "depois de conversar com o capitao nemo e sair da cela comeca a perceber como o lugar é interresante, ao ponto de ja nao mais querer ir embora, mas viver ali e fazer seus estudos nesse lugar tao rico"
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "nemo entra no seu quarto, e te chama para ir com ele fazer uma visita a sua plantacao subaquatica pessoal, e de quebra tambem voce podera estudar as especies que ele cultiva em baixo do mar",
+        alternativas: [
+            {
+                texto: "voce aceita ja que é uma oportunidade otima para estudar o fundo do mar e entender um pouco mais sobre o nemo",
+                afirmacao: [
+                    "depois de um tempo no submarino, nemo te convida para um passeio no fundo do mar, voce aceita, e la encontra uma serie de novas especis aquaticas para seus estudos",
+                    "ja estando vivendo no nautilus a algum tempo, é convidado por nemo a uma viagem subaquatica em sua floresta pessoal, voce logo aceita e vai com ele descobrindo seu deposito pessoal de perolas e plantas marinhas"
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "",
+                afirmacao: [
+                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
+                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
+                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Ferramentas de busca que utilizam IA podem facilitar muito nosso processo de busca por informações. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Utilizar informações próprias traz um toque pessoal ao nosso trabalho. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Novas tecnologias trazem novos desafios, por isso após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "É muito importante refletir sobre os trabalhadores do futuro. Por isso a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: [
+                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
+                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                ],
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: [
+                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
+                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
+                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                ],
+            },
+        ]
+    }
+];
